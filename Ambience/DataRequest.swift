f1c998bea5498818a209e90249eb755dc619d5fd
kovs705 <smartkovsik@gmail.com>
2023-06-14 17:30:18 +0300

    working on the network layer

---
+++ Ambience/DataRequest.swift
@@ -0,0 +1,45 @@
+//
+//  DataRequest.swift
+//  Ambience
+//
+//  Created by Kovs on 14.06.2023.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
+
+protocol DataRequest {
+    associatedtype Response
+    
+    var url: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String : String] { get }
+    var queryItems: [String : String] { get }
+    
+    func decode(_ data: Data) throws -> Response
+}
+
+extension DataRequest where Response: Decodable {
+    func decode(_ data: Data) throws -> Response {
+        let decoder = JSONDecoder()
+        return try decoder.decode(Response.self, from: data)
+    }
+}
+
+extension DataRequest {
+    var headers: [String : String] {
+        [:]
+    }
+    
+    var queryItems: [String : String] {
+        [:]
+    }
+}
+
---
+++ Ambience/ErrorResponse.swift
@@ -0,0 +1,26 @@
+//
+//  ErrorResponse.swift
+//  Ambience
+//
+//  Created by Kovs on 14.06.2023.
+//
+
+import Foundation
+
+enum ErrorResponse: String {
+    case apiError
+    case invalidEndpoint
+    case invalidResponse
+    case noData
+    case serializationError
+    
+    public var description: String {
+        switch self {
+        case .apiError: return "Ooops, there is something wrong with the api"
+        case .invalidEndpoint: return "Ooops, there is something wrong with the endpoint"
+        case .invalidResponse: return "Ooops, there is something wrong with the response"
+        case .noData: return "Ooops, there is something wrong with the data"
+        case .serializationError: return "Ooops, there is something wrong with the serialization process"
+        }
+    }
+}
