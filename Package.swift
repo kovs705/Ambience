0221a35def3c8503abc26e626217b79b1bc2ee44
Kovs <smartkovsik@gmail.com>
2023-11-27 11:34:02 +0300

    deleted files to untrack them

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Ambience",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Ambience",
+            targets: ["Ambience"]),
+    ],
+    
+    dependencies: [
+        // Here we define our package's external dependencies
+        // and from where they can be fetched:
+        .package(
+            url: "",
+            from: "5.6.0"
+        )
+    ],
+    
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Ambience"),
+        .testTarget(
+            name: "AmbienceTests",
+            dependencies: ["Ambience"]),
+    ]
+)
